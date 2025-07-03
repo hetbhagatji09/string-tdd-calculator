adb212871f1f2efc931109238bcde8a47c7ab8a2
hetbhagatji09 <hetbhagatji09@gmail.com>
2025-07-03 21:32:01 +0530

    Initial commit

---
+++ src/main/java/org/example/stringtddcalculator/StringTddCalculatorApplication.java
@@ -0,0 +1,13 @@
+package org.example.stringtddcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringTddCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringTddCalculatorApplication.class, args);
+	}
+
+}
